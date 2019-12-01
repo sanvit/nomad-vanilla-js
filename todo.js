afb7775bf6cc2450ddd9a430cd8fdd530939ceec
Jaewon Kim <sanvit@sanvit.me>
2019-12-01 19:56:07 +0900

    remove button

--- todo.js
+++ todo.js
@@ -1,14 +1,30 @@
 const toDoForm = document.querySelector(".js-toDoForm");
-const toDoInput = form.querySelector("input");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
-function handleSubmit(event)
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("Button");
+    delBtn.value = "X"
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintTodo(currentValue);
+    toDoInput.value = "";
+}
 
 function loadToDos(){
     const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
+    if (toDos!== null) {
         
     }
 }
@@ -17,7 +33,7 @@ function hand
 
 function init() {
     loadToDos();
-    toDoForm.addEventListener("submit", handleSubmit
+    toDoForm.addEventListener("submit", handleSubmit)
 }
 
 init();
